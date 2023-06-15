d951b9e6f7091da99a04d139501710cb9d9e74a4
PkTheCoda <107774675+PkTheCoda@users.noreply.github.com>
2023-06-15 11:41:20 -0400

    Setup all default inputs and added responsive design

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./style.css";
 import Header from "./Header.js"
 import Information from "./Information.js"
-import Preview from "./Preview.js"
 
 export default function App() {
   return (
--- src/Information.js
+++ src/Information.js
@@ -1,11 +1,129 @@
 import React from 'react'
 import { useState } from 'react'
 
-function Information() {
+function Information(props) {
   return (
-    <div className="information--holder"> 
-      Hi
-    </div>
+    <>
+      <div className="information--holder"> 
+
+        <div className="personal--holder holder">
+          <h3 className="content--title">Personal Info:</h3>
+
+          <input 
+            type="text"
+            placeholder="First Name" 
+          />
+
+          <input 
+            type="text"
+            placeholder="Last Name" 
+          />
+
+          <input 
+            type="email"
+            placeholder="Your Email" 
+          />
+
+          <input 
+            type="email"
+            placeholder="Address" 
+          />
+
+          <input 
+            type="text"
+            placeholder="Phone #" 
+          />
+
+        <textarea 
+            type="textarea"
+            placeholder="A nice description of yourself . . ." 
+          />
+
+        </div>
+
+        <div className="experience--holder holder">
+          <h3 className="content--title">Job Experience:</h3>
+
+          <input 
+            type="text"
+            placeholder="Employer" 
+          />
+
+          <input 
+            type="text"
+            placeholder="Role or Job Title" 
+          />
+
+          <div className="label-input-pair">
+            <label htmlFor="from-job">From:</label>
+            <input 
+              class="label-input"
+              id="from-job"
+              type="date" 
+            />
+          </div>
+
+          <div className="label-input-pair">
+            <label htmlFor="to-job">To:</label>
+            <input 
+              class="label-input"
+              id="to-job"
+              type="date" 
+            />
+          </div>
+
+          <input 
+            type="text"
+            placeholder="Job Location"
+          />
+
+          <textarea 
+            type="text"
+            placeholder="What were your responsibilities and accomplishments?"
+          />
+
+        </div>
+
+        <div className="education--holder holder">
+          <h3 className="content--title">Relevant Education:</h3>
+
+          <input 
+            type="text"
+            placeholder="School/University" 
+          />
+
+          <input 
+            type="text"
+            placeholder="Field of Study" 
+          />
+
+          <div className="label-input-pair">
+            <label htmlFor="grad--date">Graduation Date:</label>
+            <input 
+              class="label-input"
+              type="date"
+              placeholder="Graduation Date" 
+              id="grad--date"
+            />
+          </div>
+
+          <input 
+            type="text"
+            placeholder="Location" 
+          />
+
+          <textarea
+            placeholder="What are your academic achievements?"
+          />
+
+        </div>
+
+      </div>
+
+      <div className="preview--holder"> 
+        Hi
+      </div>
+  </>
   )
 }
 
